0131dd9f41a555366231711908ddf5dc9f23cc89
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-04 20:34:41 +0700

    chat

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const app = express();
-app.listen(3000, () => console.log('Server started'));
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3000, () => console.log('Server started'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 app.get('/', (req, res) => res.render('home'));
+io.on('connection', socket => {
+    socket.on('CLIENT_SEND_MSG', msg => {
+        io.emit('SERVER_SEND_MSG', msg);
+    });
+});
